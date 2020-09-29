8e89c10882a317f47ef628650d11382bb61cc0a3
Chip1980 <valtechBWE20>
2020-09-29 16:48:26 +0200

    Second red driver change

---
+++ src/main/java/NeedMoreMoneyException.java
@@ -0,0 +1,2 @@
+public class NeedMoreMoneyException extends RuntimeException {
+}
--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,7 +1,4 @@
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class VendingMachine {
 
@@ -10,11 +7,23 @@ public class VendingMachine {
     public DrinkResult buy(DrinkChoice drink, Coins... coin) {
         DrinkResult drinkResult = new DrinkResult();
         drinkResult.setDrink(drink);
-        drinkResult.setRest(calculateRest(coin));
+        drinkResult.setRest(calculateRest(coin, drink.cost));
         return drinkResult;
     }
 
-    private List<Coins> calculateRest(Coins[] coin) {
-        
+    private List<Coins> calculateRest(Coins[] coin, int drinkValue) {
+        int coinSum = Arrays.stream(coin).map(coins -> coins.value).reduce(0, Integer::sum);
+        if(coinSum < drinkValue) {
+            throw new NeedMoreMoneyException();
+        }
+        int diff = coinSum - drinkValue;
+        return calculateRestCoins(diff);
+    }
+
+    private List<Coins> calculateRestCoins(int diff) {
+        new TreeSet<Coins>(Coins.values());
+        while(diff > 0) {
+
+        }
     }
 }
